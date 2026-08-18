0dc722aea415b3c7912e56a905afba4a40f86438
Ronaldo Jansen <jansen.ronaldocullen@gmail.com>
2026-08-18 19:15:15 +0200

    missed

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Initialize Anthropic
 const client = new Anthropic({
